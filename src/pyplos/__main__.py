49f2b00ee2ab0f3c544a0ed9a2d8a0369c0a6d31
Jasper Zschiegner <schjaspe@amazon.de>
2023-09-15 11:33:35 +0200

    Add support for csv.

--- src/pyplos/__main__.py
+++ src/pyplos/__main__.py
@@ -1,20 +1,48 @@
-if __name__ == "__main__":
-    import json
-    import sys
+import csv
+import json
+import re
+import sys
 
-    from .util import tabulate
+from pathlib import Path
 
-    from .parser import parse_ppl
+from .parser import parse_ppl
+from .util import tabulate, valmap
 
+
+def parse(value):
+    if re.match("^\d+$", value):
+        return int(value)
+
+    try:
+        return float(value)
+    except ValueError:
+        pass
+
+    return value
+
+
+def cli():
     path, *args = sys.argv[1:]
 
-    commands = parse_ppl(" ".join(args))
+    path = Path(path)
+    if path.suffix == ".json":
+        with open(path) as infile:
+            table = json.loads(infile.read())
+    elif path.suffix == ".csv":
+        with open(path) as infile:
+            reader = csv.DictReader(infile)
 
-    with open(path) as infile:
-        table = json.loads(infile.read())
+            table = [valmap(parse, row) for row in reader]
+    else:
+        raise ValueError(f"Unknown suffix {path.suffix!r}.")
+
+    commands = parse_ppl(" ".join(args))
 
     for command in commands:
         table = command.execute(table)
 
     tabulate(table)
-    # print(table[:100])
+
+
+if __name__ == "__main__":
+    cli()
